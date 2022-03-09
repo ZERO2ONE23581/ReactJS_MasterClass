87679ac35d879de93413df242273cdc5418a6e45
aiden <zero2one23581@gmail.com>
2022-03-09 21:39:54 +0900

    adapting and extending

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="blueviolet" />
     </Father>
   );
 }
